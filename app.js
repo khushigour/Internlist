ffc34fb745dd00915b4e5f2428a3009d6dc879ff
khushigour <khuhsigour0301@gmail.com>
2020-08-18 23:12:45 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+//Book Class: Represents a BOOK
+class Intern{
+    constructor(title, link, user){
+        this.title = title;
+        this.link = link;
+        rhis.isbn = isbn;
+    }
+}
+//UI Class: Handle UI tasks
+class UI
+// Store Class: Handles storage
